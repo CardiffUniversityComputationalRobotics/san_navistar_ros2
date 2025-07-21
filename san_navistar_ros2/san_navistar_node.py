1d960a1ff024e47ba5a2ccf8d40e8428f0ad650f
sasilva1998 <sasilva1998@gmail.com>
2025-07-21 13:56:53 +0100

    feat: working demo

--- san_navistar_ros2/san_navistar_node.py
+++ san_navistar_ros2/san_navistar_node.py
@@ -3,74 +3,122 @@ import rclpy
 from rclpy.node import Node
 import torch
 from nav_msgs.msg import Odometry
-from pedsim_msgs.msg import AgentStates
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, PoseStamped
 import torch
 import torch.nn as nn
 import os
 from san_navistar_ros2.models.model import Policy
 import numpy as np
+from san_navistar_ros2.data.navigation.star.configs.config import Config
+from tf_transformations import euler_from_quaternion
+from pedsim_msgs.msg import AgentStates, AgentState
+from esc_move_base_msgs.msg import Path2D
+import gym
+from launch_ros.substitutions import FindPackageShare
+from san_navistar_ros2.utils import check_reverse
 
 
 class SANNaviStarNode(Node):
     def __init__(self):
         super().__init__("san_navistar_node")
 
+        self.san_navistar_ros2_dir = FindPackageShare("san_navistar_ros2").find(
+            "san_navistar_ros2"
+        )
+
+        # ! PARAMS DECLARATION
+        self.declare_parameter("v_pref", 0.4)
+        self.declare_parameter("robot_radius", 0.3)
+        self.declare_parameter("human_radius", 0.3)
+        self.declare_parameter("robot_fov", 3.14)
+        self.declare_parameter("human_fov", 3.14)
+        self.declare_parameter("visible_dis", 10.0)
+        self.declare_parameter("command_timer_period", 0.25)
+
+        # topics
+        self.declare_parameter("goal_path_topic", "/solution_path")
+        self.declare_parameter("social_agents_topic", "/pedsim_simulator/simulated_agents")
+        self.declare_parameter("odom_topic", "/odom")
+        self.declare_parameter("cmd_vel_topic", "/cmd_vel")
+
         self.max_human_num_ = 5
 
         self.agents_data_ = None
         self.odom_data_ = None
 
+        # ! ROBOT PARAMS
+        self.robot_radius_ = self.get_parameter("robot_radius").value
+        self.human_radius_ = self.get_parameter("human_radius").value
+        self.robot_fov_ = self.get_parameter("robot_fov").value
+        self.human_fov_ = self.get_parameter("human_fov").value
+        self.v_pref_ = self.get_parameter("v_pref").value
+
+        # goal
+        self.global_goal_x_ = 0.0
+        self.global_goal_y_ = 0.0
+
+        self.local_goal_x_ = 0.0
+        self.local_goal_y_ = 0.0
+
+        self.goal_available_ = False
+
+        self.visible_dis = self.get_parameter("visible_dis").value
+
+        self.waypoints = []
+
+        self.actor_critic_init_ = False
+        self.actor_critic = None
+
+        high = np.inf * np.ones(
+            [
+                2,
+            ]
+        )
+        self.action_space_ = gym.spaces.Box(-high, high, dtype=np.float32)
+
+        # topic configs
+        self.global_plan_topic = self.get_parameter("goal_path_topic").value
+        self.agent_states_topic = self.get_parameter("social_agents_topic").value
+        self.odom_topic = self.get_parameter("odom_topic").value
+        self.cmd_vel_topic = self.get_parameter("cmd_vel_topic").value
+
         # ! SUBSCRIBERS
         self.agent_states_sub_ = self.create_subscription(
             AgentStates,
-            "/pedsim_simulator/simulated_agents",
+            self.agent_states_topic,
             self.agent_states_callback,
             1,
         )
 
         self.odom_sub_ = self.create_subscription(
             Odometry,
-            "/odom",
+            self.odom_topic,
             self.odom_callback,
             1,
         )
 
+        self.goal_subs = self.create_subscription(
+            PoseStamped, "/goal_pose", self.global_goal_callback, 5
+        )
+
+        self.global_plan_sub = self.create_subscription(
+            Path2D, self.global_plan_topic, self.global_plan_callback, 10
+        )
+
         # ! PUBLISHERS
-        self.cmd_vel_pub_ = self.create_publisher(Twist, "cmd_vel", 10)
+        self.cmd_vel_pub_ = self.create_publisher(Twist, self.cmd_vel_topic, 10)
 
         # ! INITIALISE MODEL
 
         model_dir = "data/navigation/star"
         test_model = "00500.pt"
 
-        # from importlib import import_module
+        self.config = Config()
 
-        # if model_dir.endswith("/"):
-        #     model_dir = model_dir[:-1]
-        #     # try:
-        # model_dir_string = model_dir.replace("/", ".") + ".configs.config"
-        # print(model_dir_string)
-        # model_arguments = import_module(model_dir_string)
-        # Config = getattr(model_arguments, "Config")
-        # # except:
-        #     print(
-        #         "Failed to get Config function from ", test_args.model_dir, "/config.py"
-        #     )
-        #     from crowd_nav.configs.config import Config
-
-        from san_navistar_ros2.data.navigation.star.configs.config import Config
-
-        config = Config()
-
-        log_file = os.path.join(model_dir, "test")
-        if not os.path.exists(log_file):
-            os.mkdir(log_file)
-
-        torch.manual_seed(config.env.seed)
-        torch.cuda.manual_seed_all(config.env.seed)
-        if config.training.cuda:
-            if config.training.cuda_deterministic:
+        torch.manual_seed(self.config.env.seed)
+        torch.cuda.manual_seed_all(self.config.env.seed)
+        if self.config.training.cuda:
+            if self.config.training.cuda_deterministic:
                 torch.backends.cudnn.benchmark = False
                 torch.backends.cudnn.deterministic = True
             else:
@@ -78,29 +126,15 @@ class SANNaviStarNode(Node):
                 torch.backends.cudnn.deterministic = False
 
         torch.set_num_threads(1)
-        device = torch.device("cuda" if config.training.cuda else "cpu")
 
-        load_path = os.path.join(model_dir, "checkpoints", test_model)
-        print("load path is:", load_path)
-
-        eval_dir = os.path.join(model_dir, "eval")
-        if not os.path.exists(eval_dir):
-            os.mkdir(eval_dir)
-
-        # actor_critic = Policy(
-        #     envs.observation_space.spaces,
-        #     envs.action_space,
-        #     base_kwargs=config,
-        #     base=config.robot.policy,
-        #     device=device,
-        # )
+        self.load_path = os.path.join(model_dir, "checkpoints", test_model)
+        print("load path is:", self.load_path)
 
         # ! TIMERS
-        self.command_timer = self.create_timer(0.1, self.command_callback)
-
-        print("ready")
-
-        self.last_human_states_ = np.zeros((self.max_human_num_, 5))
+        self.command_timer_period_ = self.get_parameter("command_timer_period").value
+        self.command_timer = self.create_timer(
+            self.command_timer_period_, self.command_callback
+        )
 
     def agent_states_callback(self, msg: AgentStates):
         self.max_human_num_ = len(msg.agent_states)
@@ -109,42 +143,119 @@ class SANNaviStarNode(Node):
     def odom_callback(self, msg: Odometry):
         self.odom_data_ = msg
 
+    def global_goal_callback(self, msg: PoseStamped):
+        self.global_goal_x_ = msg.pose.position.x
+        self.global_goal_y_ = msg.pose.position.y
+        self.goal_available_ = True
+
+    def global_plan_callback(self, msg: Path2D):
+        self.waypoints = []
+
+        if len(msg.waypoints) > 1:
+
+            got_waypoints = msg.waypoints
+
+            got_waypoints.reverse()
+
+            for pos in got_waypoints:
+                if (
+                    math.sqrt(
+                        (pos.x - self.odom_data_.pose.pose.position.x) ** 2
+                        + (pos.y - self.odom_data_.pose.pose.position.y) ** 2
+                    )
+                    > 0.2
+                ):
+                    self.waypoints.append([pos.x, pos.y])
+                else:
+                    break
+
+            self.waypoints.reverse()
+        self.local_goal_x_ = self.waypoints[0][0]
+        self.local_goal_y_ = self.waypoints[0][1]
+
+        self.global_goal_x_ = self.waypoints[-1][0]
+        self.global_goal_y_ = self.waypoints[-1][1]
+
+        self.goal_available_ = True
+
     def generate_ob(self, reset):
         ob = {}
+
+        orientation_list = [
+            self.odom_data_.pose.pose.orientation.x,
+            self.odom_data_.pose.pose.orientation.y,
+            self.odom_data_.pose.pose.orientation.z,
+            self.odom_data_.pose.pose.orientation.w,
+        ]
+
+        (roll, pitch, yaw) = euler_from_quaternion(orientation_list)
 
         # nodes
         visible_humans, num_visibles, human_visibility = self.get_num_human_in_fov()
 
         self.update_last_human_states(human_visibility, reset=reset)
 
-        ob["robot_node"] = [self.robot.get_full_state_list_noV()]
+        ob["robot_node"] = np.array(
+            [
+                [
+                    [
+                        self.odom_data_.pose.pose.position.x,
+                        self.odom_data_.pose.pose.position.y,
+                        self.robot_radius_,
+                        self.local_goal_x_,
+                        self.local_goal_y_,
+                        self.v_pref_,
+                        yaw,
+                    ]
+                ]
+            ]
+        )
         # edges
         # temporal edge: robot's velocity
-        ob["temporal_edges"] = np.array([self.robot.vx, self.robot.vy]).reshape(1, -1)
+        temporal_edges = np.array(
+            [self.odom_data_.twist.twist.linear.x, self.odom_data_.twist.twist.linear.y]
+        ).reshape(1, -1)
+        ob["temporal_edges"] = np.expand_dims(temporal_edges, axis=0)
         # spatial edges: the vector pointing from the robot position to each human's position
-        spatial_edges = np.zeros((self.human_num, 2))
+        spatial_edges = np.zeros((self.max_human_num_, 2))
 
-        for i in range(self.human_num):
+        for i in range(self.max_human_num_):
             relative_pos = np.array(
                 [
-                    self.last_human_states[i, 0] - self.robot.px,
-                    self.last_human_states[i, 1] - self.robot.py,
+                    self.last_human_states_[i, 0]
+                    - self.odom_data_.pose.pose.position.x,
+                    self.last_human_states_[i, 1]
+                    - self.odom_data_.pose.pose.position.y,
                 ]
             )
             spatial_edges[i] = relative_pos
-        ob["spatial_edges"] = spatial_edges
-        ob["flatten_obs"] = np.concatenate(
+        ob["spatial_edges"] = np.array(np.expand_dims(spatial_edges, axis=0))
+        flatten_obs = np.concatenate(
             (
                 spatial_edges.reshape(
                     -1,
                 ),
-                self.robot.get_full_state_list(),
+                [
+                    self.odom_data_.pose.pose.position.x,
+                    self.odom_data_.pose.pose.position.y,
+                    self.robot_radius_,
+                    self.local_goal_x_,
+                    self.local_goal_y_,
+                    self.v_pref_,
+                    yaw,
+                ],
             ),
             axis=-1,
         )
+        ob["flatten_obs"] = np.expand_dims(flatten_obs, axis=0)
 
         goal_relative_pos = np.array(
-            [self.robot.gx - self.robot.px, self.robot.gy - self.robot.py, 1, 0]
+            [
+                self.local_goal_x_ - self.odom_data_.pose.pose.position.x,
+                self.local_goal_y_ - self.odom_data_.pose.pose.position.y,
+                1,
+                0,
+            ]
         ).reshape(1, -1)
         one_hot = np.array([0, 1] * self.max_human_num_).reshape(
             self.max_human_num_, -1
@@ -155,9 +266,17 @@ class SANNaviStarNode(Node):
         visible_mask = (dis < self.visible_dis).astype(int)
         visible_mask[-1] = 1
 
-        ob["spatial_edges_transformer"] = spatial_edges
-        ob["visible_masks"] = visible_mask
-        ob["robot_pos"] = np.array([self.robot.px, self.robot.py, 1, 0]).reshape(1, -1)
+        ob["spatial_edges_transformer"] = np.expand_dims(spatial_edges, axis=0)
+        ob["visible_masks"] = np.expand_dims(visible_mask, axis=0)
+        robot_pos = np.array(
+            [
+                self.odom_data_.pose.pose.position.x,
+                self.odom_data_.pose.pose.position.y,
+                1,
+                0,
+            ]
+        ).reshape(1, -1)
+        ob["robot_pos"] = np.expand_dims(robot_pos, axis=0)
 
         return ob
 
@@ -171,27 +290,145 @@ class SANNaviStarNode(Node):
         # keep the order of 5 humans at each timestep
         for i in range(self.max_human_num_):
             if human_visibility[i]:
-                humanS = np.array(self.humans[i].get_observable_state_list())
-                self.last_human_states[i, :] = humanS
+                # humanS = np.array(self.humans[i].get_observable_state_list())
+                humanS = np.array(
+                    [
+                        self.agents_data_[i].pose.position.x,
+                        self.agents_data_[i].pose.position.y,
+                        self.agents_data_[i].twist.linear.x,
+                        self.agents_data_[i].twist.linear.y,
+                        self.human_radius_,
+                    ]
+                )
+                self.last_human_states_[i, :] = humanS
 
             else:
                 if reset:
                     humanS = np.array([15.0, 15.0, 0.0, 0.0, 0.3])
-                    self.last_human_states[i, :] = humanS
+                    self.last_human_states_[i, :] = humanS
 
                 else:
-                    px, py, vx, vy, theta = self.last_human_states[i, :]
+                    px, py, vx, vy, theta = self.last_human_states_[i, :]
                     # Plan A: linear approximation of human's next position
-                    px = px + vx * self.time_step
-                    py = py + vy * self.time_step
-                    self.last_human_states[i, :] = np.array([px, py, vx, vy, theta])
+                    px = px + vx * self.command_timer_period_
+                    py = py + vy * self.command_timer_period_
+                    self.last_human_states_[i, :] = np.array([px, py, vx, vy, theta])
 
                     # Plan B: assume the human doesn't move, use last observation
                     # self.last_human_states[i, :] = np.array([px, py, 0., 0., r])
 
+    def get_num_human_in_fov(self):
+        human_ids = []
+        humans_in_view = []
+        num_humans_in_view = 0
+
+        for i in range(self.max_human_num_):
+            visible = self.detect_visible(self.agents_data_[i], robot1=True)
+            if visible:
+                humans_in_view.append(self.agents_data_[i])
+                num_humans_in_view = num_humans_in_view + 1
+                human_ids.append(True)
+            else:
+                human_ids.append(False)
+
+        return humans_in_view, num_humans_in_view, human_ids
+
+    def detect_visible(self, state2: AgentState, robot1=False, custom_fov=None):
+
+        orientation_list = [
+            self.odom_data_.pose.pose.orientation.x,
+            self.odom_data_.pose.pose.orientation.y,
+            self.odom_data_.pose.pose.orientation.z,
+            self.odom_data_.pose.pose.orientation.w,
+        ]
+
+        (roll, pitch, yaw) = euler_from_quaternion(orientation_list)
+
+        real_theta = yaw
+
+        # angle of center line of FOV of agent1
+        v_fov = [np.cos(real_theta), np.sin(real_theta)]
+
+        # angle between agent1 and agent2
+        v_12 = [
+            state2.pose.position.x - self.odom_data_.pose.pose.position.x,
+            state2.pose.position.y - self.odom_data_.pose.pose.position.y,
+        ]
+        # angle between center of FOV and agent 2
+
+        v_fov = v_fov / np.linalg.norm(v_fov)
+        v_12 = v_12 / np.linalg.norm(v_12)
+
+        offset = np.arccos(np.clip(np.dot(v_fov, v_12), a_min=-1, a_max=1))
+        if custom_fov:
+            fov = custom_fov
+        else:
+            if robot1:
+                fov = self.robot_fov_
+            else:
+                fov = self.human_fov_
+
+        return np.abs(offset) <= fov / 2
+
     def command_callback(self):
 
-        pass
+        if self.goal_available_ and len(self.waypoints) > 0:
+            if not self.actor_critic_init_:
+                device = torch.device("cuda" if self.config.training.cuda else "cpu")
+                self.last_human_states_ = np.zeros((self.max_human_num_, 5))
+                self.actor_critic = Policy(
+                    self.generate_ob(True),
+                    self.action_space_,
+                    base_kwargs=self.config,
+                    base=self.config.robot.policy,
+                    device=device,
+                )
+                self.actor_critic.load_state_dict(
+                    torch.load(
+                        self.san_navistar_ros2_dir
+                        + "/san_navistar_ros2/data/navigation/star/checkpoints/00500.pt",
+                        map_location=device,
+                    )
+                )
+                self.actor_critic.base.nenv = self.config.testing.num_processes
+
+                nn.DataParallel(self.actor_critic).to(device)
+                self.actor_critic_init_ = True
+                print("defined actor critic")
+
+            recurrent_cell = "GRU"
+            double_rnn_size = 2 if recurrent_cell == "LSTM" else 1
+
+            eval_recurrent_hidden_states = {}
+            eval_recurrent_hidden_states["human_node_rnn"] = np.zeros(
+                (
+                    self.config.testing.num_processes,
+                    1,
+                    self.config.SRNN.human_node_rnn_size * double_rnn_size,
+                )
+            )
+            eval_recurrent_hidden_states["human_human_edge_rnn"] = np.zeros(
+                (
+                    self.config.testing.num_processes,
+                    self.config.sim.human_num + 1,
+                    self.config.SRNN.human_human_edge_rnn_size * double_rnn_size,
+                )
+            )
+            eval_masks = np.zeros((self.config.testing.num_processes, 1))
+
+            obs = self.generate_ob(False)
+
+            _, action, _, eval_recurrent_hidden_states = self.actor_critic.act(
+                obs, eval_recurrent_hidden_states, eval_masks, deterministic=True
+            )
+
+            action = check_reverse(action)
+
+            cmd_vel = Twist()
+            cmd_vel.linear.x = float(action[0][0])
+            cmd_vel.angular.z = float(action[0][1])
+
+            self.cmd_vel_pub_.publish(cmd_vel)
 
 
 def main(args=None):
--- setup.py
+++ setup.py
@@ -11,6 +11,22 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
+        (
+            os.path.join(
+                "share",
+                package_name,
+                "san_navistar_ros2",
+                "data",
+                "navigation",
+                "star",
+                "checkpoints",
+            ),
+            glob(
+                os.path.join(
+                    "san_navistar_ros2/data/navigation/star/checkpoints", "*.pt"
+                )
+            ),
+        ),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
